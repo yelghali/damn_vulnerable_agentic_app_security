d44a34f1fd7bd881b74b90652b8b5cd05c7bb7c8
Yassine El Ghali <Yassine.el.ghali@microsoft.com>
2026-05-30 01:56:06 +0200

    infra: rename deprecated key vault enable_rbac_authorization to rbac_authorization_enabled

--- src/infra/keyvault.tf
+++ src/infra/keyvault.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name        = azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
-  enable_rbac_authorization  = true
+  rbac_authorization_enabled = true
   purge_protection_enabled   = false
   soft_delete_retention_days = 7
 
